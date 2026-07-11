8f4619d43887b78893b80840b1b16d7c267f3d71
ribeck96 <riannonbeck@gmail.com>
2026-07-11 12:02:04 -0400

    Create catalog.js

---
+++ catalog.js
@@ -0,0 +1,25 @@
+const catalog = document.getElementById("catalog");
+
+degrees.forEach(function(degree) {
+
+    const card = document.createElement("div");
+
+    card.className = "degreeCard";
+
+    card.innerHTML = `
+        <h3>${degree.level} of ${degree.type}</h3>
+
+        <h2>${degree.degree}</h2>
+
+        <p><strong>${degree.college}</strong></p>
+
+        <p>${degree.description}</p>
+
+        <p><strong>${degree.books}</strong> Books Required</p>
+
+        <button>View Degree</button>
+    `;
+
+    catalog.appendChild(card);
+
+});
