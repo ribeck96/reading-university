14159d244418f0959308f50a9931305a5fdc0042
ribeck96 <riannonbeck@gmail.com>
2026-07-11 13:47:38 -0400

    Update degreeCatalog.js

--- degreeCatalog.js
+++ degreeCatalog.js
@@ -1,25 +1,36 @@
 const degrees = [
-{
 
-    id: "assoc-biology",
+    {
+        id: "assoc-biology",
+        level: "Associate",
+        college: "College of Natural Sciences",
+        degree: "Biology",
+        type: "Science",
+        books: 4,
+        description: "Study living things through fiction and nonfiction.",
+        suggested: [
+            "BIO101",
+            "BIO120",
+            "BIO205",
+            "GEN101"
+        ]
+    },
 
-    level: "Associate",
+    {
+        id: "bachelor-history",
+        level: "Bachelor",
+        college: "College of Humanities",
+        degree: "History",
+        type: "Arts",
+        books: 8,
+        description: "Explore humanity's past through historical works.",
+        suggested: [
+            "Ancient History",
+            "American History",
+            "World History",
+            "Research",
+            "Elective"
+        ]
+    }
 
-    college: "College of Natural Sciences",
-
-    degree: "Biology",
-
-    type: "Science",
-
-    books: 4,
-
-    description: "Study living things through fiction and nonfiction.",
-
-    suggested: [
-        "BIO101",
-        "BIO120",
-        "BIO205",
-        "GEN101"
-    ]
-}
 ];
