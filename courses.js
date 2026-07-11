1d8f2faa7a4cfa29e946b3662bb322705ee1bd35
ribeck96 <riannonbeck@gmail.com>
2026-07-11 11:52:40 -0400

    Create courses.js

---
+++ courses.js
@@ -0,0 +1,21 @@
+const courses = [
+
+{
+    code: "BIO101",
+    name: "Animal Biology",
+    description: "Read books centered on animals."
+},
+
+{
+    code: "BIO102",
+    name: "Marine Biology",
+    description: "Read books about oceans or marine life."
+},
+
+{
+    code: "BIO103",
+    name: "Genetics",
+    description: "Read books focused on family, heredity, or ancestry."
+}
+
+];
