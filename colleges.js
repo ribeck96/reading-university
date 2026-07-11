b8aca5fa013b4e61bff1c1ea648a55193bb8f980
ribeck96 <riannonbeck@gmail.com>
2026-07-11 12:53:21 -0400

    Create colleges.js

---
+++ colleges.js
@@ -0,0 +1,15 @@
+const colleges = [
+
+{
+    id: "natural-sciences",
+    name: "College of Natural Sciences",
+    description: "Programs exploring the natural world."
+},
+
+{
+    id: "humanities",
+    name: "College of Humanities",
+    description: "Programs exploring human culture and history."
+}
+
+];
