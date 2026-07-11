8c1f6201deca4d6fe27146f2c07baca8b218c9d5
ribeck96 <riannonbeck@gmail.com>
2026-07-11 11:46:28 -0400

    Create degrees.js

---
+++ degrees.js
@@ -0,0 +1,30 @@
+const degrees = [
+
+{
+    level: "Associate",
+    major: "Biology",
+    books: 4,
+    description: "Study living things through fiction and nonfiction.",
+    suggested: [
+        "Animal Biology",
+        "Marine Biology",
+        "Genetics",
+        "General Education"
+    ]
+},
+
+{
+    level: "Bachelor",
+    major: "History",
+    books: 8,
+    description: "Explore humanity's past through historical works.",
+    suggested: [
+        "Ancient",
+        "American",
+        "World",
+        "Research",
+        "Elective"
+    ]
+}
+
+];
