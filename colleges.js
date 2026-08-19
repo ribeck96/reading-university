ece9d27184b672e255f4fc33090b39a5c086303e
ribeck96 <riannonbeck@gmail.com>
2026-08-19 14:14:29 -0400

    Update colleges.js

--- colleges.js
+++ colleges.js
@@ -11,5 +11,21 @@ const colleges = [
     name: "College of Humanities",
     description: "Programs exploring human culture and history."
 }
+  
+{
+    id: "social-sciences",
+    name: "College of Social Sciences",
+    description: "Programs devoted to the study of societies and the relationships among members within those societies."
+}
+{
+    id: "fine-arts",
+    name: "College of Fine Arts",
+    description: "Programs exploring high art made primarily for aesthetics or creative expression."
+}
+{
+    id: "mysterious",
+    name: "College of The Mysterious",
+    description: "Programs exploring subjects beyond typical university offerings."
+}
 
 ];
