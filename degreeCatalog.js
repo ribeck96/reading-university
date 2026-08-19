c0759167bfb1862b4b214da36ac2c91ab60d1222
ribeck96 <riannonbeck@gmail.com>
2026-08-19 15:16:47 -0400

    Update degreeCatalog.js

--- degreeCatalog.js
+++ degreeCatalog.js
@@ -7,7 +7,7 @@ const degrees = [
         degree: "Biology",
         type: "Science",
         books: 4,
-        description: "Study living things through fiction and nonfiction.",
+        description: "Study living things and how they interact in the world.",
 
         suggested: [
             "biologyElective",
@@ -16,8 +16,71 @@ const degrees = [
             "generalEducation"
         ]
     },
+    {
+        id: "assoc-chemistry",
+        level: "Associate",
+        college: "College of Natural Sciences",
+        degree: "Chemistry",
+        type: "Science",
+        books: 4,
+        description: "The study of matter and its properties.",
 
+        suggested: [
 
+        ]
+    },
+    {
+        id: "assoc-physics",
+        level: "Associate",
+        college: "College of Natural Sciences",
+        degree: "Physics",
+        type: "Science",
+        books: 4,
+        description: "The study of matter, energy, space, and time.",
+
+        suggested: [
+       
+        ]
+    },
+        {
+        id: "assoc-geology",
+        level: "Associate",
+        college: "College of Natural Sciences",
+        degree: "Geology",
+        type: "Science",
+        books: 4,
+        description: "The study of ROCKS.",
+
+        suggested: [
+       
+        ]
+    },
+        {
+        id: "assoc-englit",
+        level: "Associate",
+        college: "College of Humanities",
+        degree: "English Literature",
+        type: "Humanities",
+        books: 4,
+        description: "The study of literature in the English language.",
+
+        suggested: [
+       
+        ]
+    },
+        {
+        id: "assoc-english",
+        level: "Associate",
+        college: "College of Humanities",
+        degree: "English Composition",
+        type: "Humanities",
+        books: 4,
+        description: "To read is to be a writer.",
+
+        suggested: [
+       
+        ]
+    },
     {
         id: "bachelor-history",
         level: "Bachelor",
