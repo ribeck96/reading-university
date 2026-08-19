cfc45d280f70d21020704fd03d574c517739584f
ribeck96 <riannonbeck@gmail.com>
2026-08-19 13:34:13 -0400

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB7qZaOR8YuM00vWqsr0fq8Rw6UMaLWgWA",
+  authDomain: "stevens-university-4f6ea.firebaseapp.com",
+  projectId: "stevens-university-4f6ea",
+  storageBucket: "stevens-university-4f6ea.firebasestorage.app",
+  messagingSenderId: "621087391900",
+  appId: "1:621087391900:web:e7d0bf8f04fd03a9d1ad59"
+};
